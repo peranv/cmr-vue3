0fb027897a613d152d077401ba241b642260cb00
Esperanza Najera <esperanza.nave@gmail.com>
2023-07-06 10:27:36 -0700

    configurando api rest

--- src/views/NuevoClienteView.vue
+++ src/views/NuevoClienteView.vue
@@ -8,6 +8,14 @@
             type:String
         }
     })
+
+    const formData = {
+        nombre:'Esperanza'
+    }
+
+    const handelSubmit = (data)=>{
+        console.log(data)
+    }
 </script>
 
 <template>
@@ -21,14 +29,61 @@
       <div class="mx-auto mt-10 bg-white shadow">
         <div class="mx-auto md:w-2/3 py-20 px-6">
             <FormKit
-                   type="form">
+                   type="form"
+                   submit-label="Agregar cliente"
+                   incomplete-message="No se pudo enviar, revisa los mensajes"
+                   @submit="handelSubmit"
+                   :value="formData"
+            >
 
                    <FormKit 
                         type="text"  
                         label="Nombre"
+                        name="nombre"
                         placeholder="Nombre de Cliente"
                         validation="required"
                         :validation-messages="{required: 'El Nombre del Cliente es Obligatorio'}"
+                   />
+
+                   <FormKit 
+                        type="text"  
+                        label="Apellido"
+                        name="apellido"
+                        placeholder="Apellido de Cliente"
+                        validation="required"
+                        :validation-messages="{required: 'El Apellido del Cliente es Obligatorio'}"
+                   />
+
+                   <FormKit 
+                        type="email"  
+                        label="Email"
+                        name="email"
+                        placeholder="Email de Cliente"
+                        validation="required|email"
+                        :validation-messages="{required: 'El Email del Cliente es Obligatorio', email:'Coloca un email válido'}"
+                   />
+
+                   <FormKit 
+                        type="text"  
+                        label="Teléfono"
+                        name="telefono"
+                        placeholder="Teléfono: XXX-XXX-XXXX"
+                        validation="*matches:/^[0-9]{3}-[0-9]{3}-[0-9]{4}$/"
+                        :validation-messages="{matches: 'El formato no es válido'}"
+                   />
+
+                   <FormKit 
+                        type="text"  
+                        label="Empresa"
+                        name="empresa"
+                        placeholder="Empresa de Cliente"
+                   />
+
+                   <FormKit 
+                        type="text"  
+                        label="Puesto"
+                        name="puesto"
+                        placeholder="Puesto de Cliente"
                    />
 
           </FormKit>
