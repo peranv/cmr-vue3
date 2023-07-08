acefdf42f3f0f58df580fc0f7873283266fa311d
Esperanza Najera <esperanza.nave@gmail.com>
2023-07-07 19:05:52 -0700

    revisar si tenemos clientes y se agrego la tabla de clientes

--- src/views/NuevoClienteView.vue
+++ src/views/NuevoClienteView.vue
@@ -1,4 +1,5 @@
 <script setup>
+    
     import { FormKit } from '@formkit/vue'
     import RouterLink from '../components/UI/RouterLink.vue';
     import Heading from '../components/UI/Heading.vue';
